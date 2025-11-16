980c7715ef8de4728ce8b5b5be78cdf4e3f4bc2b
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2025-11-16 13:49:46 -0500

    get formatted result with dominant emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,24 @@
-import requests # Import the requests library to handle HTTP requests
+import requests, json, operator # Import the requests library to handle HTTP requests
 
 def emotion_detector(text_to_analyse):  # Define a function named emotion_detector that takes a string input (text_to_analyse)
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  # URL of the emotion prediction service
     myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
-    return response.text
+    formatted_response = json.loads(response.text)
+    data = formatted_response["emotionPredictions"][0]["emotion"]
+    anger = data["anger"]
+    disgust = data["disgust"]
+    fear = data["fear"]
+    joy = data["joy"]
+    sadness = data["sadness"]
+    result = {
+        "anger": anger,
+        "disgust": disgust,
+        "fear": fear,
+        "joy": joy,
+        "sadness": sadness,        
+    }
+    dominant_emotion = max(result.items(), key=operator.itemgetter(1))[0]
+    result["dominant_emotion"] = dominant_emotion    
+    return result
