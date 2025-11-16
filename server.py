3047f420ee5382cd7fa439cea178cb07f2b58a26
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2025-11-16 15:00:40 -0500

    added docstrings, removed trailing white space, deindented code

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+''' Executing this function initiates the application of emotion
+    detector to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +9,11 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emot_detector():
+    ''' This code receives the text from the HTML interface and 
+        runs emotion detection over it using emotion_detector()
+        function. The output returned shows the emotions and their
+        scores for the provided text.
+    '''
     user_text = request.args.get('textToAnalyze')
     response = emotion_detector(user_text)
     anger = response['anger']
@@ -16,13 +25,16 @@ def emot_detector():
 
     if dominant_emotion is None:
         return "Invalid text! Please try again!"
-    else:
-        return f"For the given statement, the system response is 'anger': {anger}, \
-        'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. \
-        The dominant emotion is {dominant_emotion}."
+
+    return f"For the given statement, the system response is 'anger': {anger}, \
+    'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. \
+    The dominant emotion is {dominant_emotion}."
 
 @app.route("/")
 def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
