cfa183447705a27d565021542cd209dcd7b468d7
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2025-11-16 14:18:33 -0500

    return statement formatted to several lines

--- server.py
+++ server.py
@@ -14,7 +14,8 @@ def emotion_detector():
     sadness = response['sadness']
     dominant_emotion = response['dominant_emotion']
 
-    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
+    return f"For the given statement, the system response is 'anger': {anger}, \
+    'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
 
 @app.route("/")
 def render_index_page():
