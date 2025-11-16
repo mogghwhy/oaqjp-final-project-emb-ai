484b012922b6d7f42be5b181f13baf2df965bf80
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2025-11-16 14:50:03 -0500

    added dominant_emotion None value handling

--- server.py
+++ server.py
@@ -14,9 +14,12 @@ def emot_detector():
     sadness = response['sadness']
     dominant_emotion = response['dominant_emotion']
 
-    return f"For the given statement, the system response is 'anger': {anger}, \
-    'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. \
-    The dominant emotion is {dominant_emotion}."
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
+    else:
+        return f"For the given statement, the system response is 'anger': {anger}, \
+        'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. \
+        The dominant emotion is {dominant_emotion}."
 
 @app.route("/")
 def render_index_page():
